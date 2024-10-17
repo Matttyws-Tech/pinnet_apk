d6934eb78e7ea9eb0cfbca791427791e27413d9d
Matttyws-Tech <nemowigeta2@gmail.com>
2024-10-17 11:34:57 -0500

    01 version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Browser } from '@capacitor/browser';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss'],  
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit{
+  constructor() {}    
+  
+  ngOnInit(): void{
+    
+    Browser.open({ url: 'http://192.168.0.100:4200/' });  // URL de tu aplicación Angular    
+    Browser.addListener('browserFinished', ()=>{
+      App.exitApp()
+    });
+  }
 }
